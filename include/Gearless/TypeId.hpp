011a44b10a03a377ca4e6cfa8b7b113b1c834c96
TheArtist <agorglouk@gmail.com>
2015-07-16 23:26:38 +0300

    Implemented templated type id generator

---
+++ include/Gearless/TypeId.hpp
@@ -0,0 +1,80 @@
+/*********************************************************************************************************************/
+/*                                                  /===-_---~~~~~~~~~------____                                     */
+/*                                                 |===-~___                _,-'                                     */
+/*                  -==\\                         `//~\\   ~~~~`---.___.-~~                                          */
+/*              ______-==|                         | |  \\           _-~`                                            */
+/*        __--~~~  ,-/-==\\                        | |   `\        ,'                                                */
+/*     _-~       /'    |  \\                      / /      \      /                                                  */
+/*   .'        /       |   \\                   /' /        \   /'                                                   */
+/*  /  ____  /         |    \`\.__/-~~ ~ \ _ _/'  /          \/'                                                     */
+/* /-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`                                                      */
+/*                   \_|      /        _)   ;  ),   __--~~                                                           */
+/*                     '~~--_/      _-~/-  / \   '-~ \                                                               */
+/*                    {\__--_/}    / \\_>- )<__\      \                                                              */
+/*                    /'   (_/  _-~  | |__>--<__|      |                                                             */
+/*                   |0  0 _/) )-~     | |__>--<__|     |                                                            */
+/*                   / /~ ,_/       / /__>---<__/      |                                                             */
+/*                  o o _//        /-~_>---<__-~      /                                                              */
+/*                  (^(~          /~_>---<__-      _-~                                                               */
+/*                 ,/|           /__>--<__/     _-~                                                                  */
+/*              ,//('(          |__>--<__|     /                  .----_                                             */
+/*             ( ( '))          |__>--<__|    |                 /' _---_~\                                           */
+/*          `-)) )) (           |__>--<__|    |               /'  /     ~\`\                                         */
+/*         ,/,'//( (             \__>--<__\    \            /'  //        ||                                         */
+/*       ,( ( ((, ))              ~-__>--<_~-_  ~--____---~' _/'/        /'                                          */
+/*     `~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/                                                     */
+/*   ._-~//( )/ )) `                    ~~-'_/_/ /~~~~~~~__--~                                                       */
+/*    ;'( ')/ ,)(                              ~~~~~~~~~~                                                            */
+/*   ' ') '( (/                                                                                                      */
+/*     '   '  `                                                                                                      */
+/*********************************************************************************************************************/
+#ifndef _TYPEID_HPP_
+#define _TYPEID_HPP_
+
+#include <type_traits>
+
+template<class T, class U=
+    typename std::remove_cv<
+        typename std::remove_pointer<
+            typename std::remove_reference<
+                typename std::remove_extent<T>::type
+                >::type
+            >::type
+        >::type
+    >
+struct remove_all : remove_all<U> {};
+template<class T> struct remove_all<T, T> { typedef T type; };
+
+namespace Gearless
+{
+    using TypeId = unsigned long;
+
+    class TypeIdGenBase
+    {
+        protected:
+            static TypeId GenTypeId()
+            {
+                static TypeId nextId = 0;
+                return nextId++;
+            }
+    };
+
+    template<typename T>
+    class TypeIdGen : public TypeIdGenBase
+    {
+        public:
+            static TypeId GetTypeId()
+            {
+                static TypeId tId = TypeIdGenBase::GenTypeId();
+                return tId;
+            }
+    };
+
+    template<typename T>
+    TypeId GetTypeId()
+    {
+        return TypeIdGen<remove_all<T>::type>::GetTypeId();
+    }
+}
+
+#endif // ! _TYPEID_HPP_
