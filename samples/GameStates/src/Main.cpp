8ef69d18c4712f6cf9500f61a403a04ddcd590c6
TheArtist <agorglouk@gmail.com>
2016-03-11 13:50:38 +0200

    Corrected and made GameState sample more verbose

--- samples/GameStates/src/Main.cpp
+++ samples/GameStates/src/Main.cpp
@@ -48,11 +48,34 @@ using InitState = MainMenu;
 ///==============================================================
 ///= Transition Actions
 ///==============================================================
-void OnLoading(const LoadGame&) { std::cout << "Loading in Game resources..." << std::endl; }
-void OnMainGameStart(const StartGame&) { std::cout << "Starting the Game..." << std::endl; }
-void OnPause(const PauseGame&) { std::cout << "Game is paused." << std::endl; }
-void OnResume(const ResumeGame&) { std::cout << "Game is resumed." << std::endl; }
-void OnMainMenuReturn(const GoToMainMenu&) { std::cout << "Returning to MainMenu..." << std::endl; }
+// Current game screen
+std::string curScreen = "MENU_SCREEN";
+// Transition actions
+void OnLoading(const LoadGame&)
+{
+    std::cout << "Loading in Game resources..." << std::endl;
+    curScreen = "LOADING_SCREEN";
+}
+void OnMainGameStart(const StartGame&)
+{
+    std::cout << "Starting the Game..." << std::endl;
+    curScreen = "MAIN_GAME_SCREEN";
+}
+void OnPause(const PauseGame&)
+{
+    std::cout << "Game is paused." << std::endl;
+    curScreen = "PAUSE_SCREEN";
+}
+void OnResume(const ResumeGame&)
+{
+    std::cout << "Game is resumed." << std::endl;
+    curScreen = "MAIN_GAME_SCREEN";
+}
+void OnMainMenuReturn(const GoToMainMenu&)
+{
+    std::cout << "Returning to MainMenu..." << std::endl;
+    curScreen = "MENU_SCREEN";
+}
 
 ///==============================================================
 ///= Transition Table
@@ -90,15 +113,25 @@ using InitState = MainMenu;
 ///= Sample Usage
 ///==============================================================
 
+void ShowCurrentScreen() { std::cout << "Current screen is: " << curScreen << std::endl; }
+
 void Test()
 {
     Gearless::StateMachine<InitState, TransitionTbl> sm;
     sm.Start();
+    ShowCurrentScreen();
     sm.ProcessEvent(LoadGame());
+    ShowCurrentScreen();
+    sm.ProcessEvent(StartGame());
+    ShowCurrentScreen();
     sm.ProcessEvent(PauseGame());
+    ShowCurrentScreen();
     sm.ProcessEvent(ResumeGame());
+    ShowCurrentScreen();
     sm.ProcessEvent(PauseGame());
+    ShowCurrentScreen();
     sm.ProcessEvent(GoToMainMenu());
+    ShowCurrentScreen();
     sm.Stop();
 }
 
